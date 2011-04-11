89af2bc123c47e8a963404db677f2f7c6a80c1fa
Harshad Joshi <firewalrus@gmail.com>
2011-04-11 04:55:33 -0700

    Added GNU License Notice to code..

--- gtalkautoreplybot.py
+++ gtalkautoreplybot.py
@@ -9,6 +9,21 @@
 # ToDo         - Add a GUI maybe, and write more good code
 # Contains some code from  - Patrick Archibald..http://b.patrickarchibald.com/2010/06/17/email-notification-of-twitter-mentions/
 
+#This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 
 import xmpp
 import smtplib
