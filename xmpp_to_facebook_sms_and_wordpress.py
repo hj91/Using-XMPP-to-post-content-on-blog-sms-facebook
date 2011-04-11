e99eb2a7bf5523706d96184411bd1db59f98f1b9
Harshad Joshi <firewalrus@gmail.com>
2011-04-11 04:56:35 -0700

    GNU License Notice added.

--- xmpp_to_facebook_sms_and_wordpress.py
+++ xmpp_to_facebook_sms_and_wordpress.py
@@ -22,6 +22,20 @@
 # 	        - Add some more scalability to the bot, ie instead of hardcoding the blog/user/passwd within the        program, 
 #               - should ask for it once and store it in the backend.
 
+#This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 blog_name = 'yourblognameatwordpress/xmlrpc.php'
 user_name = 'username'
