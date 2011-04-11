8926dce91b82a7c186dc0ae52d35f0a740cfe649
Harshad Joshi <firewalrus@gmail.com>
2011-04-11 04:49:07 -0700

    Added GNU License notice in the code.

--- send_sms_to_get_gmail.py
+++ send_sms_to_get_gmail.py
@@ -1,6 +1,20 @@
 ## check-and-get-gmail-on-phone.py -- A command line util to check GMail and get it on mobile -*- Python -*-
 ## Harshad Joshi (c) 2011.
-## This program is provided 'as-is-what-was' condition...use this code in your program only if you are planning to release it as opensource..
+
+# This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 import sys
 import urllib             # For BasicHTTPAuthentication
