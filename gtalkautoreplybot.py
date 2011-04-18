6ecfa4eb54e0315a8632a830411736bf8e37c2d1
Harshad Joshi <firewalrus@gmail.com>
2011-04-18 06:33:22 -0700

    Edited gtalkautoreplybot.py via GitHub

--- gtalkautoreplybot.py
+++ gtalkautoreplybot.py
@@ -7,10 +7,7 @@
 # Features     - Unicode enabled
 #
 # ToDo         - Add a GUI maybe, and write more good code
-<<<<<<< HEAD:gtalkautoreplybot.py
-=======
 # Contains some code from  - Patrick Archibald..http://b.patrickarchibald.com/2010/06/17/email-notification-of-twitter-mentions/
->>>>>>> slave1:gtalkautoreplybot.py
 
 
 import xmpp
