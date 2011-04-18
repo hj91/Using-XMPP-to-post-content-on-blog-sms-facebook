81b954a850c95a0d8be8218ea5da5d2056590760
Harshad Joshi <firewalrus@gmail.com>
2011-04-18 06:39:46 -0700

    Edited send_sms_to_get_gmail.py via GitHub

--- send_sms_to_get_gmail.py
+++ send_sms_to_get_gmail.py
@@ -2,6 +2,16 @@
 ## Harshad Joshi (c) 2011.
 ## This program is provided 'as-is-what-was' condition...use this code in your program only if you are planning to release it as opensource..
 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
 import sys
 import urllib             # For BasicHTTPAuthentication
 import feedparser         # For parsing the feed
